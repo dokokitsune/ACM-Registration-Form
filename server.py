bdf7a9661bbe3296a74d6c695ba662a98efab71b
sahis2002 <89941586+SahisN@users.noreply.github.com>
2023-06-23 11:45:32 -0700

    linking part 1

--- server.py
+++ server.py
@@ -13,13 +13,35 @@ def main_page():
 def submit_form():
     print("submit form")
     if request.method == "POST":
-        data = request.form.to_dict()
-        write_to_csv(data)
-        print(data["project-workshop"])
+        # data = request.form.to_dict()
+        # print(request.form.getlist("availability"))
+        # print(data)
+        get_data(request)
+
+        # write_to_csv(data)
         return "Submitted"
 
 
 def write_to_csv(data):
+    email = data["Email"]
+    membership_status = data["membership"]
+    first_name = data["firstName"]
+    last_name = data["lastName"]
+    id = data["cin"]
+    phone_number = data["phone-number"]
+    discord_tag = data["discordTag"]
+    gender = data["gender"]
+    major = data["major"]
+    grade = data["standing"]
+    senior_design = data["senior-design"]
+    graduation_year = data["gradyear"]
+    hear = data["hear"]
+    buddy_system = data["buddy"]
+    project_workshop = data["project-workshop"]
+    avalibility = data["avalibility"]
+    suggestion = data["recommendations"]
+    confirm_email = data["confirmation"]
+
     with open("database.csv", newline="", mode="a") as csv_database:
         email, message, subject = (
             data["firstName"],
@@ -30,3 +52,46 @@ def write_to_csv(data):
             csv_database, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
         )
         csv_writer.writerow([email, subject, message])
+
+
+def get_data(request):
+    data = request.form.to_dict()
+    print(data)
+    """email = data['Email']
+    membership_status = data["membership"]
+    first_name = data["firstName"]
+    last_name = data["lastName"]
+    id = data["cin"]
+    phone_number = data["phone-number"]
+    discord_tag = data["discordTag"]
+    gender = data["gender"]
+    major = data["major"]
+    grade = data['standing']
+    senior_design = data['senior-design']
+    graduation_year = data['gradyear']
+    hear = data['hear']
+    career_expectation = data['gain']
+    buddy_system = data['buddy']
+    project_workshop = data['project-workshop']
+    avalibility = data['avalibility']
+    suggestion = data['recommendations']
+    confirm_email = data['confirmation']"""
+
+    hear = request.form.getlist("hear")
+    career_expectation = request.form.getlist("gain")
+    project_workshop = request.form.getlist("project-workshop")
+    availability = request.form.getlist("availability")
+
+    print(extract_from_list(hear))
+    print(extract_from_list(career_expectation))
+    print(extract_from_list(project_workshop))
+    print(extract_from_list(availability))
+
+
+# Takes an array and returns a string
+def extract_from_list(arr):
+    result = ""
+    for word in arr:
+        result += word + " "
+
+    return result
