cfb55018349bb8c188190c75e01a2cd9857ffb42
dokokitsune <stakin101@gmail.com>
2024-08-07 17:08:04 -0700

    updated form to 2024-2025 and testing session-id

--- server.py
+++ server.py
@@ -1,16 +1,41 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, abort
+from flask_cors import CORS
+import stripe
+from urllib import parse
+from functools import wraps
 from utilities import utility, writer
 from mail.EmailBot import MailBot
 from sheets.googlesheets import addData
 from mail.HtmlTemplate import HtmlTemplate
 
+stripe.api_key = "pk_live_51H0yOZEr4ylg7vlAnEDF4YfjfRe1VAEKjRMuW2Lh7zlMG9Lh68k4LZmuTm0RtR5MeNLJzkxUT0p53pdnQKgeIY1800N4Sipf5y"
+
 app = Flask(__name__)
+CORS(app, resourses={r"/*": {"origins": "https://acm-calstatela.com/"}})
 
-
+@app.before_request
+def check_referrer():
+    allowed_hosts = ["https://acm-calstatela.com", "127.0.0.1:5000"]
+    referrer = request.headers.get("Referer")
+    if referrer:
+        referrer_host = parse.urlparse(referrer).netloc
+        if referrer_host not in allowed_hosts:
+            abort(403)
 @app.route("/")
 def main_page():
     return render_template("index.html")
 
+@app.route('/success')
+def success():
+    session_id = request.args.get('session_id')
+    try:
+        session = stripe.checkout.Session.retrieve(session_id)
+        if session.payment_status == "paid":
+            return render_template("index.html")
+        else:
+            return "Payment not completed.", 400
+    except stripe.error.InvalidRequestError:
+        return "Invalid session ID", 400
 
 @app.route("/submit_form", methods=["POST", "GET"])
 def submit_form():
